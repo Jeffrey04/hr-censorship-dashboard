fc45ec5e629d6b7d61ad2ca00aa6e9fbe73b3c62
Jeffrey04 <mycyberpet@yahoo.com>
2018-07-24 22:57:56 +0800

    add data table example

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,10 +5,12 @@ import 'font-awesome/css/font-awesome.css';
 
 import {Card} from 'primereact/card';
 import {Chart} from 'primereact/components/chart/Chart';
+import {DataTable} from 'primereact/datatable';
+import {Column} from 'primereact/column';
 
 class App extends Component {
     render() {
-        const data = {
+        const data_radar = {
             labels: [
                 'Eating',
                 'Drinking',
@@ -41,13 +43,41 @@ class App extends Component {
                 }
             ]
         };
+        const data_table = {
+            data: [
+                {brand: 'VW', year: 2012, color: 'Orange', vin: 'dsad231ff'},
+                {brand: 'Audi', year: 2011, color: 'Black', vin: 'gwregre345'},
+                {brand: 'Renault', year: 2005, color: 'Gray', vin: 'h354htr'},
+                {brand: 'BMW', year: 2003, color: 'Blue', vin: 'j6w54qgh'},
+                {
+                    brand: 'Mercedes',
+                    year: 1995,
+                    color: 'Orange',
+                    vin: 'hrtwy34'
+                },
+                {brand: 'Volvo', year: 2005, color: 'Black', vin: 'jejtyj'},
+                {brand: 'Honda', year: 2012, color: 'Yellow', vin: 'g43gr'},
+                {brand: 'Jaguar', year: 2013, color: 'Orange', vin: 'greg34'},
+                {brand: 'Ford', year: 2000, color: 'Black', vin: 'h54hw5'},
+                {brand: 'Fiat', year: 2013, color: 'Red', vin: '245t2s'}
+            ]
+        };
 
         return (
             <div className="ui-g">
                 <div className="ui-g-2" />
                 <div className="ui-g-8">
                     <Card title="PrimeReact" subTitle="Radar Chart Demo">
-                        <Chart type="radar" data={data} />
+                        <Chart type="radar" data={data_radar} />
+                    </Card>
+
+                    <Card title="PrimeReact" subTitle="Data Table Demo">
+                        <DataTable value={data_table.data}>
+                            <Column field="vin" header="Vin" />
+                            <Column field="year" header="Year" />
+                            <Column field="brand" header="Brand" />
+                            <Column field="color" header="Color" />
+                        </DataTable>
                     </Card>
                 </div>
             </div>
